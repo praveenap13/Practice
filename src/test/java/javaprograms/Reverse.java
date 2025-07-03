a153a01e542fa0694422966b3913503ddc5c2257
praveena <pravina.parab@gmail.com>
2025-07-03 19:35:33 +0530

    25

--- src/test/java/javaprograms/Reverse.java
+++ src/test/java/javaprograms/Reverse.java
@@ -7,7 +7,7 @@ public class Reverse {
         while(num!=0){
             //rev=rev*10+num%10;
             System.out.print(num%10);
-            num= num/10;
+            num= num/0;
         }
         System.out.println();
         System.out.println(rev);
